2cacbe06208edb7297254fdd8fbade8fd60f9971
조민철 <fclemonschool@gmail.com>
2022-05-15 22:33:24 +0900

    ClimbingStairs

---
+++ src/io/fclemonschool/leetCode/ClimbingStairs.java
@@ -0,0 +1,43 @@
+package io.fclemonschool.leetCode;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ * Example 1:
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ *
+ * Example 2:
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ * Constraints:
+ * 1 <= n <= 45
+ */
+public class ClimbingStairs {
+    protected static final int[] dp = new int[46];
+    public static int climbStairs(int n) {
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 1;
+        }
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
+        return dp[n];
+    }
+
+    private ClimbingStairs() {
+    }
+}

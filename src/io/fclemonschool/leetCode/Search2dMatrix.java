2242fea91cf971231c0b7f99f25f0ebca2549525
조민철 <fclemonschool@gmail.com>
2022-05-18 21:31:00 +0900

    Search2dMatrix

---
+++ src/io/fclemonschool/leetCode/Search2dMatrix.java
@@ -0,0 +1,41 @@
+package io.fclemonschool.leetCode;
+
+/***
+ * Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix.
+ * This matrix has the following properties:
+ *
+ * Integers in each row are sorted from left to right.
+ * The first integer of each row is greater than the last integer of the previous row.
+ *
+ * Example 1:
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+ * Output: true
+ *
+ * Example 2:
+ * Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+ * Output: false
+ *
+ * Constraints:
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 100
+ * -104 <= matrix[i][j], target <= 104
+ */
+public class Search2dMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int num = matrix[0].length - 1;
+        boolean result = false;
+        for (int[] i : matrix) {
+            if (i[0] <= target && i[num] >= target) {
+                for (int j : i) {
+                    if (j == target) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
